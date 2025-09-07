ea517c4b75dda1f2d13ed68bcfdf6826fbfa88d2
David Vujic <david@vujic.se>
2025-09-07 16:49:00 +0200

    fix(render): add raw content helper function

--- src/python_hiccup/html/__init__.py
+++ src/python_hiccup/html/__init__.py
@@ -1,5 +1,5 @@
 """Render data into HTML."""
 
-from python_hiccup.html.core import render
+from python_hiccup.html.core import raw, render
 
-__all__ = ["render"]
+__all__ = ["raw", "render"]
--- test/test_render_html.py
+++ test/test_render_html.py
@@ -1,6 +1,6 @@
 """Unit tests for the python_hiccup.html.render function."""
 
-from python_hiccup.html import render
+from python_hiccup.html import raw, render
 
 
 def test_returns_a_string() -> None:
@@ -146,6 +146,7 @@ def test_order_of_items() -> None:
 
 def test_content_as_function() -> None:
     """Allow defining content as a callable function, as a custom parser."""
-    data = ["div", lambda: "&copy;"]
+    content = "&copy; this <strong>should</strong> not be escaped!"
 
-    assert render(data) == "<div>&copy;</div>"
+    assert render(["div", raw("&copy;")]) == "<div>&copy;</div>"
+    assert render(["div", raw(content)]) == f"<div>{content}</div>"
